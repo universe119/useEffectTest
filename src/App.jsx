423f7b1b00649dcd3982bcb63817454d29c89641
universe119 <ruddh119@gmail.com>
2024-10-04 01:28:28 +0900

    주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,9 @@ export default function App() {
 	// 	});
 
 	useEffect(() => {
+		//의존성 배열이 비어있는 useEffect문의 콜백함수는 아무리 해당 컴포넌트가 재랜더링되더라도
+		//처음 마운트시 한번만 호출되므로 fetch함수가 처음 한번만 데이터를 가져오고 더이상 무한호출에 빠지지 않음
+		//실무에서 React-Query를 쓰기 전까지는 제일 많이 쓰이는 리액트 개발 패턴 (중요)
 		fetch("/member.json")
 			.then(data => data.json())
 			.then(json => {
